39a65dc11561012447edf57b414cd974eac68ce3
0gnjen01 <ognjenk0l3@gmail.com>
2026-01-16 18:53:46 +0100

    microbin.env

--- nixos/selfhost/microbin.nix
+++ nixos/selfhost/microbin.nix
@@ -11,10 +11,7 @@
         MICROBIN_BIND = "127.0.0.1";
         MICROBIN_PORT = 8080;
         MICROBIN_PUBLIC_PATH = "https://microbin.1gnis.me";
-        MICROBIN_ADMIN_USERNAME = "1gnis";
-        MICROBIN_ADMIN_PASSWORD = "$(${pkgs.coreutils}/bin/cat ${config.sops.secrets.microbin_password.path})";
         MICROBIN_READONLY = true;
-        MICROBIN_UPLOADER_PASSWORD = "$(${pkgs.coreutils}/bin/cat ${config.sops.secrets.microbin_password.path})";
         MICROBIN_ENCRYPTION_CLIENT_SIDE = true;
         MICROBIN_ENCRYPTION_SERVER_SIDE = true;
         MICROBIN_HASH_IDS = true;
@@ -42,5 +39,4 @@
     defaults.email = "ognjenk0l3@gmail.com";
     certs."microbin.1gnis.me" = {};
   };
-  sops.secrets.microbin_password = {};
 }
--- secrets/sops.nix
+++ secrets/sops.nix
@@ -3,6 +3,8 @@
     defaultSopsFile = ./secrets.yaml;
     defaultSopsFormat = "yaml";
 
+    secrets.microbin_env.sopsFile = ./microbin.env.env;
+
     age.keyFile = "/home/ignis/.config/sops/age/keys.txt";
   };
 }
