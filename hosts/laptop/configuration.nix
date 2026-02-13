77dfc6ea9827b4e8637b9275dd18338b516112c2
0gnjen01 <ognjenk0l3@gmail.com>
2026-02-13 18:36:11 +0100

    shit

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -27,23 +27,23 @@
       HandleLidSwitchExternalPower = "ignore";
       HandleLidSwitchDocked = "ignore";
     };
-  };
-  tlp = {
-    enable = true;
-    settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "balanced";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "balanced";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "balanced";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "balanced";
 
-      CPU_MIN_PERF_ON_AC = 0;
-      CPU_MAX_PERF_ON_AC = 100;
-      CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 50;
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 50;
 
-      START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
-      STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+        START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
+        STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+      };
     };
   };
 
--- modules/nixos/selfhost/nextcloud.nix
+++ modules/nixos/selfhost/nextcloud.nix
@@ -12,27 +12,12 @@
 
     services.nextcloud = {
       enable = true;
-      hostName = "nextcloud.1gnis.me";
+      hostName = "nixos";
       package = pkgs.nextcloud32;
       config.adminpassFile = "${config.sops.secrets.nextcloud_password.path}";
       config.dbtype = "sqlite";
-      https = true;
-      settings = {
-        trusted_domains = ["nextcloud.1gnis.me"];
-      };
-    };
 
-    sops.secrets.nextcloud_password = {};
-
-    services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
-      forceSSL = true;
-      enableACME = true;
-    };
-
-    security.acme = {
-      certs = {
-        ${config.services.nextcloud.hostName}.email = "ognjenk0l3@gmail.com";
-      };
+      sops.secrets.nextcloud_password = {};
     };
   };
 }
